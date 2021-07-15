da4bd678b5d94ea23bd8f6a827e9e31744cd3799
Javier Varona <javier@contimita.com>
2021-07-14 22:00:08 -0400

    - The dogenode now downloads the bootstrap for dogecoin

--- extras/docker/dogecoin/start.sh
+++ extras/docker/dogecoin/start.sh
@@ -3,6 +3,16 @@
 # Launch, utilizing the SIGTERM/SIGINT propagation pattern from
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
 : ${PARAMS:=""}
+if [ ! -f /root/.dogecoin/blocks/blk00000.dat ]; then
+    echo "Downloading dogecoin bootstrap..."
+    wget -O /root/.dogecoin/dogecoin-bootstrap.tar.gz https://dogeparty.net/bootstrap/dogecoin-bootstrap.tar.gz
+	echo "Extracting dogecoin bootstrap..."
+    pv /root/.dogecoin/dogecoin-bootstrap.tar.gz | tar -xz -C /root/.dogecoin/
+	echo "Deleting dogecoin bootstrap tar gz..."
+    rm /root/.dogecoin/dogecoin-bootstrap.tar.gz
+	rm /root/.dogecoin/checksums.txt
+fi
+
 trap 'kill -TERM $PID' TERM INT
 /usr/local/bin/dogecoind ${PARAMS} $@ &
 PID=$!
